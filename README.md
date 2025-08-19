fd9d37397270e49cdfb8f7e658c3be1134d2f69e
Mohamamd Tahir <153367147+mtbaloch@users.noreply.github.com>
2025-08-19 21:37:01 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Backend Project Setup - Minimal Setup
+
+
+<!-- initialize the project -->
+
+run following command in terminal
+npm init OR npm init -y
+
+
+<!-- Install ExpressJS (Nodejs framework) -->
+
+npm i express
---
+++ src/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+
+
+// initialize the express
+// Creates an Express application. The express() function is a top-level function exported by the express module
+const app = express();
+
+const apiUrl = {
+  "current_user_url": "https://api.github.com/user",
+  "current_user_authorizations_html_url": "https://github.com/settings/connections/applications{/client_id}",
+  "authorizations_url": "https://api.github.com/authorizations",
+  "code_search_url": "https://api.github.com/search/code?q={query}{&page,per_page,sort,order}",
+  "commit_search_url": "https://api.github.com/search/commits?q={query}{&page,per_page,sort,order}",
+  "emails_url": "https://api.github.com/user/emails",
+  "emojis_url": "https://api.github.com/emojis",
+  "events_url": "https://api.github.com/events",
+  "feeds_url": "https://api.github.com/feeds",
+  "followers_url": "https://api.github.com/user/followers",
+  "following_url": "https://api.github.com/user/following{/target}",
+  "gists_url": "https://api.github.com/gists{/gist_id}",
+  "hub_url": "https://api.github.com/hub",
+  "issue_search_url": "https://api.github.com/search/issues?q={query}{&page,per_page,sort,order}",
+  "issues_url": "https://api.github.com/issues",
+  "keys_url": "https://api.github.com/user/keys",
+  "label_search_url": "https://api.github.com/search/labels?q={query}&repository_id={repository_id}{&page,per_page}",
+  "notifications_url": "https://api.github.com/notifications",
+  "organization_url": "https://api.github.com/orgs/{org}",
+  "organization_repositories_url": "https://api.github.com/orgs/{org}/repos{?type,page,per_page,sort}",
+  "organization_teams_url": "https://api.github.com/orgs/{org}/teams",
+  "public_gists_url": "https://api.github.com/gists/public",
+  "rate_limit_url": "https://api.github.com/rate_limit",
+  "repository_url": "https://api.github.com/repos/{owner}/{repo}",
+  "repository_search_url": "https://api.github.com/search/repositories?q={query}{&page,per_page,sort,order}",
+  "current_user_repositories_url": "https://api.github.com/user/repos{?type,page,per_page,sort}",
+  "starred_url": "https://api.github.com/user/starred{/owner}{/repo}",
+  "starred_gists_url": "https://api.github.com/gists/starred",
+  "topic_search_url": "https://api.github.com/search/topics?q={query}{&page,per_page}",
+  "user_url": "https://api.github.com/users/{user}",
+  "user_organizations_url": "https://api.github.com/user/orgs",
+  "user_repositories_url": "https://api.github.com/users/{user}/repos{?type,page,per_page,sort}",
+  "user_search_url": "https://api.github.com/search/users?q={query}{&page,per_page,sort,order}"
+}
+// REST API Style / Architecture 
+// Representational State Transfer
+
+// First Route - Health Check Route
+// we create this to test our server, either it's running or not
+app.get("/",(req, res)=>{
+  // there is not data in body of request when using get http methods. It's idempotance method
+  // console.log(req.route.path)
+  res.status(200).json({"status":true, "message":"success","data":apiUrl})
+});
+
+/**
+ * app - instance of express(). starting point of our backend server / application
+ * get - http method (get, post, patch, put, delete, head, options)
+ * "/" - path parameter (static, dynamic, query)
+ * ()=>{} - path controller function - here we write the business logic
+ */
+
+app.get("/create-user",(req,res)=>{
+  res.status(201).json({"status":true, "message":"Use created successfully."})
+})
+// forms, api testing tools (postman, insomnia, swaggerui)
+
+
+
+// I need to listen my app on localhost
+app.listen(3000,()=>{
+  console.log(`Server is running on http://localhost:3000`)
+})
